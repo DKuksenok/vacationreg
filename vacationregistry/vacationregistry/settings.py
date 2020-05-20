2cce63e8d0a4f05b04d14ae94715561a33c84049
Dmitry Kuksenok <d.kuksenok@outlook.com>
2020-05-21 01:39:43 +0300

    Edit settings.py for staticfiles

--- vacationregistry/vacationregistry/settings.py
+++ vacationregistry/vacationregistry/settings.py
@@ -131,7 +131,11 @@ USE_TZ = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
 
 STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
