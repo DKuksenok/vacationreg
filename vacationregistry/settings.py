a4f550fef294b128450b73e3cbd9d267da63325a
Dmitry Kuksenok <d.kuksenok@outlook.com>
2020-05-21 02:10:26 +0300

    Edit ALLOWED_HOSTS for decouple

--- vacationregistry/settings.py
+++ vacationregistry/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
-from decouple import config, UndefinedValueError
+from decouple import config, UndefinedValueError, Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv)
 
 
 # Application definition
